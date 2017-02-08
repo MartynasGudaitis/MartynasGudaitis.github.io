7dd5a0cba5bed3f9b84cc93c11c0680a4e1420e9
Martynas <martynas.gudai@gmail.com>
2017-02-08 03:04:27 -0800

    Only 1 button functional

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var i = 0;
+var sum = 0e-2;
+var netPlan1 = 9.98;
+var total = document.getElementById("total");
+
+function Add() {
+	if(i==0)
+	{
+		sum +=netPlan1;
+		i++;
+	}
+	else{
+		sum -=netPlan1;
+		i--;
+	}
+	total.innerHTML = "Suma<br>" + String(sum) + " €";
+}
