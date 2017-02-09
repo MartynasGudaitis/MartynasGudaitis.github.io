45507725535ee37a76deed385def8e0f9aa3959c
Martynas <martynas.gudai@gmail.com>
2017-02-09 14:22:08 +0200

    All buttons works fine but needs recoding and styling

--- js/main.js
+++ js/main.js
@@ -5,31 +5,29 @@ var activePlans = [0, 0, 0, 0, 0, 0];
 function AddSubtract(planId, activness){
 	var myPlan = document.getElementById(planId);
 	if(activness == 0){
-		myPlan.className += "active"; activness++;
+		myPlan.className += "activated"; activness++;
 	} else {
-		myPlan.className -= "active"; activness--;
+		myPlan.className -= "activated"; activness--;
 	}
 	return activness;
 }
 
+
+
 function WhichOfThreePlans(isActive1, isActive2, isActive3, planId1, planId2, planId3){
 	if (isActive1 == 0 && isActive2 == 0 && isActive3 == 0) {
-		document.getElementById(planId1).className += "active";
-		console.log('all were equal to zero'); 
+		document.getElementById(planId1).className += "activated";
 		isActive1++;
-
 	} else if (isActive1 == 0 && isActive2 == 1 && isActive3 == 0) {
-		document.getElementById(planId1).className += "active"; 
+		document.getElementById(planId1).className += "activated"; 
 		isActive1++;
-		document.getElementById(planId2).className -= "active"; 
+		document.getElementById(planId2).className -= "activated"; 
 		isActive2--;
-		console.log('isActive2 was active. isActive1 ' + isActive1 + ' isActive2 ' + isActive2);
 	} else if (isActive1 == 0 && isActive2 == 0 && isActive3 == 1) {
-		document.getElementById(planId1).className += "active"; 
+		document.getElementById(planId1).className += "activated"; 
 		isActive1++;
-		document.getElementById(planId3).className -= "active"; 
+		document.getElementById(planId3).className -= "activated"; 
 		isActive3--;
-		console.log('isActive3 was active. isActive1 ' + isActive1 + ' isActive3 ' + isActive3);
 	}
 	return [isActive1, isActive2, isActive3];
 }
@@ -40,24 +38,40 @@ function Add(_id){
 
 	switch (_id) {
 	case planIds[0]:
-		var pln = new Array();
-		pln = WhichOfThreePlans(activePlans[0], activePlans[1], activePlans[2], planIds[0], planIds[1], planIds[2]);
-		for(var i = 0; i <=2; i++)
-			activePlans[i] = pln[i];
+			if (activePlans[0] == 1) {
+				document.getElementById(_id).className -= "activated"; activePlans[0]--;
+			}
+			else{
+				var pln = new Array();
+				pln = WhichOfThreePlans(activePlans[0], activePlans[1], activePlans[2], planIds[0], planIds[1], planIds[2]);
+				activePlans[0] = pln[0];
+				activePlans[1] = pln[1];
+				activePlans[2] = pln[2];
+			}
 		break;
 	case planIds[1]:
+		if (activePlans[1] == 1) {
+			document.getElementById(_id).className -= "activated"; activePlans[1]--;
+		}
+		else{
 		var pln = new Array();
 		pln = WhichOfThreePlans(activePlans[1], activePlans[0], activePlans[2], planIds[1], planIds[0], planIds[2]);
 		activePlans[1] = pln[0];
 		activePlans[0] = pln[1];
 		activePlans[2] = pln[2];
+		}
 		break;
 	case planIds[2]:
-		var pln = new Array();
-		pln = WhichOfThreePlans(activePlans[2], activePlans[1], activePlans[0], planIds[2], planIds[1], planIds[0]);
-		activePlans[2] = pln[0];
-		activePlans[1] = pln[1];
-		activePlans[0] = pln[2];
+		if (activePlans[1] == 1) {
+			document.getElementById(_id).className -= "activated"; activePlans[2]--;
+		}
+		else{
+			var pln = new Array();
+			pln = WhichOfThreePlans(activePlans[2], activePlans[1], activePlans[0], planIds[2], planIds[1], planIds[0]);
+			activePlans[2] = pln[0];
+			activePlans[1] = pln[1];
+			activePlans[0] = pln[2];
+		}
 		break;
 	case planIds[3]:
 		activePlans[3] = AddSubtract(planIds[3], activePlans[3]);
@@ -71,7 +85,7 @@ function Add(_id){
 	default:
 		console.log('Some kind of error.');
 		break;
-	}
+}
 	for (var i = 0; i <= activePlans.length-1; i++) {
 		console.log(activePlans.toString());
 		if(activePlans[i] == 1 )
